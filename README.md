cea7a05c3d3ddb5c17589a065aebbfed5fe826c9
Shouduo <handsomesaywhynot@gmail.com>
2016-10-15 21:50:11 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a demo of Database practice.
+
+![](http://i.imgur.com/S5Ifh9I.png)
---
+++ app/src/main/java/com/shouduo/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,53 @@
+package com.shouduo.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by 刘亨俊 on 2016/5/13.
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_BOOK = "create table Book ("
+            + "id integer primary key autoincrement, "
+            + "author text, "
+            + "price real, "
+            + "pages integer, "
+            + "name text, "
+            + "category_id_integer)";
+
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)";
+
+//    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+//        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
+//        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL("drop table if exists Book");
+//        db.execSQL("drop table if exists Category");
+//        onCreate(db);
+
+        switch (oldVersion) {
+            case 1:
+                db.execSQL(CREATE_CATEGORY);
+            case 2:
+                db.execSQL("alter table Book add column category_id_integer");
+            default:
+        }
+    }
+}
