a87ae4322e6908d7a3aa5181dfa9dff9f0c16ac9
Shouduo <handsomesaywhynot@gmail.com>
2016-10-17 15:52:51 +0800

    Use ContentProvider.

--- app/src/main/java/com/shouduo/databasetest/MainActivity.java
+++ app/src/main/java/com/shouduo/databasetest/MainActivity.java
@@ -2,7 +2,7 @@ package com.shouduo.databasetest;
 
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,114 +11,68 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MyDatabaseHelper dbHelper;
+    private String newId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
-
-        Button createDatabase = (Button) findViewById(R.id.create_database);
         Button addData = (Button) findViewById(R.id.add_data);
-        Button updateData = (Button) findViewById(R.id.update_data);
-        Button deleteData = (Button) findViewById(R.id.delete_data);
-        Button queryData = (Button) findViewById(R.id.query_data);
-        Button replaceData = (Button) findViewById(R.id.replace_data);
-
-        createDatabase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dbHelper.getWritableDatabase();
-            }
-        });
-
         addData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Uri uri = Uri.parse("content://com.shouduo.databasetest.provider/book");
                 ContentValues values = new ContentValues();
-
-                values.put("name", "The Da Vinci Code");
-                values.put("author", "Dan Brown");
-                values.put("pages", 454);
-                values.put("price", 16.96);
-                db.insert("Book", null, values);
-                values.clear();
-
-                values.put("name", "The Lost Symbol");
-                values.put("author", "Dan Brown");
-                values.put("pages", 510);
-                values.put("price", 19.95);
-                db.insert("Book", null, values);
-
-//                Toast.makeText(MainActivity.this, "insert successful", Toast.LENGTH_SHORT).show();
-
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
             }
         });
 
-        updateData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                ContentValues values = new ContentValues();
-                values.put("price", 10.99);
-                db.update("Book", values, "name = ?", new String[]{"The Da Vinci Code"});
-            }
-        });
-
-        deleteData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                db.delete("Book", "pages > ?", new String[]{"500"});
-            }
-        });
-
+        Button queryData = (Button) findViewById(R.id.query_data);
         queryData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                Cursor cursor = db.query("Book", null, null, null, null, null, null);
-                if (cursor.moveToFirst()) {
-                    do {
+                Uri uri = Uri.parse("content://com.shouduo.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
                         String name = cursor.getString(cursor.getColumnIndex("name"));
                         String author = cursor.getString(cursor.getColumnIndex("author"));
                         int pages = cursor.getInt(cursor.getColumnIndex("pages"));
                         double price = cursor.getDouble(cursor.getColumnIndex("price"));
-
                         Log.d("MainActivity", "book name is " + name);
                         Log.d("MainActivity", "book author is " + author);
                         Log.d("MainActivity", "book pages is " + pages);
                         Log.d("MainActivity", "book price is " + price);
-                    } while (cursor.moveToNext());
+                    }
+                    cursor.close();
                 }
-                cursor.close();
             }
         });
 
-        replaceData.setOnClickListener(new View.OnClickListener() {
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                db.beginTransaction();
-                try {
-                    db.delete("Book", null, null);
-//                    if (true) {
-//                        throw new NullPointerException();
-//                    }
-                    ContentValues values = new ContentValues();
-                    values.put("name", "Game of Thrones");
-                    values.put("author", "George Martin");
-                    values.put("pages", 720);
-                    values.put("price", 20.85);
-                    db.insert("Book", null, values);
-                    db.setTransactionSuccessful();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    db.endTransaction();
-                }
+                Uri uri = Uri.parse("content://com.shouduo.databasetest.provider/book/" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "A Storm of Swords");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.shoudou.databasetest.provider/book/" + newId);
+                getContentResolver().delete(uri, null, null);
             }
         });
     }
